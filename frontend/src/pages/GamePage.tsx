7180fae9b97533e820aeeb72e16ea6bf04220dca
iambariz <dmbarkanyi@gmail.com>
2026-04-02 07:51:17 +0100

    Added pages

---
+++ frontend/src/pages/GamePage.tsx
@@ -0,0 +1,9 @@
+const GamePage = () => {
+    return (
+        <div className="game-page">
+        <h1>Welcome to Game Page!</h1>
+        </div>
+    );
+};
+
+export default GamePage;
---
+++ frontend/src/pages/LobbyPage.tsx
@@ -0,0 +1,9 @@
+const LobbyPage = () => {
+    return (
+        <div className="lobby-page">
+        <h1>Welcome to Lobby Page!</h1>
+        </div>
+    );
+};
+
+export default LobbyPage;
